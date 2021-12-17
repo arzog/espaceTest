2beb033ee2e117ad731ffc277e1b4297cf8afa8b
azerious <lol-attitude666@hotmail.com>
2021-12-17 10:05:49 +0100

    ENFIN BORDEL DE MERDE

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,95 +1,75 @@
+import java.util.ArrayList;
+
 public class Test {
 	final static int[][] GAME_BOARD = new int[5][7];
-	final static int NBR_BOMBS = 5;
-	final static int[][] BOMB_POS = new int[NBR_BOMBS][2];
+	final static int NBR_BOMBS = 3;
+	final static ArrayList<int[]> BOMB_POS = new ArrayList<int[]>();
 
 	public static void main(String[] args) {
-		/*
-		 * int[][] myTab = new int[9][5]; int[][] BOMB_POS = new int[5][2]; int x_player
-		 * = myTab[0].length / 2; int y_player = myTab.length / 2; int tempBombX = 0;
-		 * int tempBombY = 0;
-		 * 
-		 * for (int i = 0; i < myTab.length; i++) {
-		 * 
-		 * for (int j = 0; j < myTab[0].length; j++) { do { tempBombX =
-		 * Utilities.randomInt(0, myTab[0].length); tempBombY = Utilities.randomInt(0,
-		 * myTab.length); } while (tempBombX == x_player && tempBombY == y_player);
-		 * 
-		 * BOMB_POS[j][1] = tempBombX; BOMB_POS[j][0] = tempBombY; }
-		 * 
-		 * }
-		 * 
-		 * for (int i = 0; i < BOMB_POS.length; i++) { for (int j = 1; j <
-		 * BOMB_POS[0].length; j++) {
-		 * 
-		 * System.out.print("(" + BOMB_POS[i][0] + "," + BOMB_POS[i][1] + ") ");
-		 * 
-		 * } System.out.println(); }
-		 * 
-		 * System.out.println("Y player : " + y_player + ", X player : " + x_player);
-		 * System.out.println(isInTabMultiDimension(x_player, y_player, BOMB_POS));
-		 * 
-		 * }
-		 * 
-		 * public static boolean isInTabMultiDimensionV2(int x_player, int y_player, int
-		 * BOMB_POS[][]) {
-		 * 
-		 * int similarY = 0;
-		 * 
-		 * similarY = Utilities.isInTabV2(y_player, BOMB_POS);
-		 * 
-		 * if (similarY != -1) { int i = 0; while (i < BOMB_POS[0].length) { if
-		 * (x_player == BOMB_POS[similarY][i]) { return true; } i++; } return false;
-		 * 
-		 * } else { return false; }
-		 */
 
 		int x_player = GAME_BOARD[0].length / 2;
 		int y_player = GAME_BOARD.length / 2;
 
-		
+		System.out.println(GAME_BOARD.length);
+		setBombsPositions(x_player, y_player, BOMB_POS);
 		showBombsPositions(x_player, y_player, BOMB_POS);
-		System.out.println(x_player + "," + y_player);
 		letsPlay(x_player, y_player, BOMB_POS);
 
 	}
 
-	public static void showBombsPositions(int x_player, int y_player, int BOMB_POS[][]) {
+	public static void setBombsPositions(int x_player, int y_player, ArrayList<int[]> BOMB_POS) {
 
-		setBombsPositions(x_player, y_player, BOMB_POS);
-		for (int i = 0; i < BOMB_POS.length; i++) {
+		int tempBombX = 0;
+		int tempBombY = 0;
+		boolean isAlreadyExisting = false;
+		boolean isSamePositionPlayer = false;
+		boolean validation = false;
 
-			System.out.print("(" + BOMB_POS[i][0] + "," + BOMB_POS[i][1] + ") ");
+		for (int i = 0; i < NBR_BOMBS; i++) {
+
+			do {
+				tempBombX = Utilities.randomInt(0, GAME_BOARD[0].length - 1);
+				tempBombY = Utilities.randomInt(0, GAME_BOARD.length - 1);
+				isSamePositionPlayer = (tempBombX == x_player && tempBombY == y_player) ? true : false;
+				isAlreadyExisting = isInTab(tempBombX, tempBombY, BOMB_POS);
+
+				if (isSamePositionPlayer) {
+					validation = true;
+				} else {
+					if (isAlreadyExisting) {
+						validation = true;
+					} else {
+						validation = false;
+					}
+				}
+			} while (validation);
+
+			int bombPos[] = { tempBombY, tempBombX };
+
+			BOMB_POS.add(bombPos);
 
 		}
+
+	}
+
+	public static void showBombsPositions(int x_player, int y_player, ArrayList<int[]> BOMB_POS) {
+
+		for (int i = 0; i < NBR_BOMBS; i++) {
+
+			System.out.print("(" + BOMB_POS.get(i)[1] + "," + BOMB_POS.get(i)[0] + ") ");
+
+		}
+
 		System.out.println();
 
 	}
 
-	public static void setBombsPositions(int x_player, int y_player, int[][] BOMB_POS) {
-
-		int tempBombX = 0;
-		int tempBombY = 0;
-
-		for (int i = 0; i < BOMB_POS.length; i++) {
-
-			do {
-				tempBombX = Utilities.randomInt(0, GAME_BOARD[0].length);
-				tempBombY = Utilities.randomInt(0, GAME_BOARD.length);
-			} while (tempBombX == x_player && tempBombY == y_player);
-
-			BOMB_POS[i][1] = tempBombX;
-			BOMB_POS[i][0] = tempBombY;
-
-		}
-
-	}
-
-	public static void letsPlay(int x_player, int y_player, int BOMB_POS[][]) {
+	public static void letsPlay(int x_player, int y_player, ArrayList<int[]> BOMB_POS) {
 
 		String playerMove = "";
 		int tempX_player = x_player;
 		int tempY_player = y_player;
+		boolean validation = false;
 
 		do {
 			playerMove = Utilities.getUserSpecificInput(
@@ -109,9 +89,19 @@ public class Test {
 
 			}
 
-		} while ((!isInTabMultiDimension(tempX_player, tempY_player, BOMB_POS)) || tempX_player < 0 );
+			if (isInTab(tempX_player, tempY_player, BOMB_POS)) {
+				validation = false;
+			} else {
+				if (tempX_player <= 0 || tempY_player <= 0 || tempX_player >= GAME_BOARD[0].length
+						|| tempY_player >= GAME_BOARD.length) {
+					validation = false;
+				} else {
+					validation = true;
+				}
+			}
 
-		if (isInTabMultiDimension(tempX_player, tempY_player, BOMB_POS)) {
+		} while (validation);
+		if (isInTab(tempX_player, tempY_player, BOMB_POS)) {
 
 			System.out.println("Bombe touchée - Vous avez perdu!");
 
@@ -123,31 +113,18 @@ public class Test {
 
 	}
 
-	public static boolean isInTabMultiDimension(int x_player, int y_player, int BOMB_POS[][]) {
+	public static boolean isInTab(int x_player, int y_player, ArrayList<int[]> BOMB_POS) {
 
-		int similarY = 0;
-
-		similarY = isInTabV2(y_player, BOMB_POS);
-
-		if (similarY != -1 && x_player == BOMB_POS[similarY][0]) {
-			return true;
-
-		} else {
-			return false;
-		}
-
-	}
-
-	static int isInTabV2(int inputNumber, int Nombres[][]) {
-		int i = 0;
-
-		while (i < Nombres.length) {
-			if (inputNumber == Nombres[i][1]) {
-				return i;
+		for (int i = 0; i < BOMB_POS.size(); i++) {
+			if (y_player == BOMB_POS.get(i)[0]) {
+				if (x_player == BOMB_POS.get(i)[1]) {
+					return true;
+				}
 			}
-			i++;
+
 		}
-		return -1;
+		return false;
+
 	}
 
 }
